49a793b9d1755e616cc05aa780007973bae8cae4
Roseline <roselynbassey23@gmail.com>
2024-05-08 14:00:34 +0100

    updated main file

--- programming-in-GO-book/main.go
+++ programming-in-GO-book/main.go
@@ -4,4 +4,27 @@ import "fmt"
 
 func main() {
 	fmt.Println("1 + 1 =", 1 + 1)
-}
+
+	str := `this is a
+multiline
+string`
+		
+	fmt.Println(str)
+
+	tabs := `this string
+            will have tabs 
+            in it`
+
+	fmt.Println(tabs)
+	fmt.Println(len("Hello world!"))
+	fmt.Println("hello world"[2])
+	fmt.Println("Hello " + "world")
+
+	//boolean
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}      
